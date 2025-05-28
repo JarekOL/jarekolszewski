9ec966b17ee9b81e58e3a2280acc61c48e6cb17e
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-05-28 17:26:05 +0200

    dfg

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: "https://jarekolszewski.pl",
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-    changefreq: "daily",
-    priority: 0.7,
-    trailingSlash: false,
-    additionalPaths: async () => {
-        return [{ loc: "/sesja-slubna-w-gorach" }];
-    },
+const config = {
+  siteUrl: "https://jarekolszewski.pl",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 0.7,
+  trailingSlash: false,
+  additionalPaths: async () => {
+    const { getAllBlogPaths } = await import("./lib/getAllBlogPaths.js");
+    return await getAllBlogPaths();
+  },
 };
+
+export default config;
