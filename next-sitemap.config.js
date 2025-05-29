711bd7cd5523187d1a7d8b4d7a3376f6bc3677cd
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-05-29 10:29:03 +0200

    df

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: "https://jarekolszewski.pl",
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  changefreq: "daily",
-  priority: 0.7,
-  trailingSlash: false,
-  additionalPaths: async () => {
-    const { getAllBlogPaths } = await import("./lib/getAllBlogPaths.js");
-    return await getAllBlogPaths();
+    siteUrl: "https://jarekolszewski.pl",
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: "daily",
+    priority: 0.7,
+    trailingSlash: false,
+    additionalPaths: async () => {
+    const { getAllBlogPaths, getAllPageSlugs } = await import("./lib/getAllPaths.js");
+
+    const blogPaths = await getAllBlogPaths();
+    const pagePaths = await getAllPageSlugs();
+
+    return [...blogPaths, ...pagePaths];
   },
 };
 
