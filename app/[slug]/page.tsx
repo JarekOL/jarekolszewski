99e405b431ac213bb1ff8c056926157e9167620b
Valkon <96625039+Nawija@users.noreply.github.com>
2025-06-06 01:25:35 +0200

    ds

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -50,4 +50,4 @@ export default async function Page({
     }
 
     return <UniversalPage content={content} />;
-}
+} 
--- lib/getPageDataFromMarkdown.ts
+++ lib/getPageDataFromMarkdown.ts
@@ -16,7 +16,7 @@ export async function getPageDataFromMarkdown(slug: string) {
     return {
         metadata: {
             alternates: {
-                canonical: `https://www.jarekolszewski.pl/${slug}`,
+                canonical: `https://jarekolszewski.pl/${slug}`,
             },
             title: data.title,
             description: data.description,
